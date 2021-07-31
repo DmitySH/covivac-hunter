c8b37c5795cd6025ec31e37ab059bc8de3b34c24
dmitry <di.im2015@yandex.ru>
2021-07-31 10:30:48 +0300

    script done

--- get_covivac.py
+++ get_covivac.py
@@ -1,4 +1,8 @@
+import re
 import smtplib
+from bs4 import BeautifulSoup
+import requests
+import time
 
 
 def send_mail(sender, password, recipient, subject, text):
@@ -11,5 +15,28 @@ def send_mail(sender, password, recipient, subject, text):
 
 
 if __name__ == '__main__':
-    send_mail('emailsender9221@gmail.com', 'alexei4!!', 'di.im2015@yandex.ru',
-              'testTitle', 'testemessage')
+    headers = {
+        'Accept': '*/*',
+        'User - Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+    }
+    url = 'https://www.mos.ru/city/projects/covid-19/privivka/'
+    time_online = 0
+
+    while True:
+        if time_online % 1440 == 0:
+            send_mail('emailsender9221@gmail.com', 'alexei4!!', 'di.im2015@yandex.ru',
+                      'Online', 'Still online')
+        try:
+            req = requests.get(url, headers=headers)
+            src = req.text
+            soup = BeautifulSoup(src, 'lxml')
+            vaccine_info = soup.find(text=re.compile('нет в наличии'))
+            if not vaccine_info:
+                for i in range(4):
+                    send_mail('emailsender9221@gmail.com', 'alexei4!!', 'di.im2015@yandex.ru',
+                              'Covivac', 'Something happened!')
+        except Exception as ex:
+            send_mail('emailsender9221@gmail.com', 'alexei4!!', 'di.im2015@yandex.ru',
+                      'Exception', 'Something went wrong!')
+        time.sleep(1800)
+        time_online += 30
